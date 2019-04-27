1e6731bd90304105841a564d68062183e6d027dd
tayloa <halfnote1004@gmail.com>
2019-04-26 20:54:36 -0400

    Setup file structure for api

---
+++ server/app.js
@@ -0,0 +1,59 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const errorHandler = require('errorhandler');
+const cors = require('cors');
+const snoowrap = require('snoowrap');
+const port = 3000;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Configure app
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+// Add routes
+app.use(require('./routes'));
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+
+const server = app.listen((process.env.port || 3000), () => console.log(`[FRESH]Finder is listening on port ${port}!`));
---
+++ server/routes/api/index.js
@@ -0,0 +1,98 @@
+require('dotenv').config();
+const router = require('express').Router();
+const snoowrap = require('snoowrap');
+
+// console.log(options);
+// const searchOptions = {
+//   subreddit: "HipHopHeads",
+//   query: "[FRESH ",
+//   sort: "relevance",
+//   time: "week"
+// }
+// const searchOptions = {
+//   subreddit: "RnBHeads",
+//   query: "[FRESH ",
+//   sort: "relevance",
+//   time: "week"
+// }
+// const searchOptions = {
+//   subreddit: "rnb",
+//   query: "[FRESH ",
+//   sort: "relevance",
+//   time: "week"
+// }
+// const searchOptions = {
+//   subreddit: "PopHeads",
+//   query: "[FRESH ",
+//   sort: "relevance",
+//   time: "week"
+// }
+// const searchOptions = {
+//   subreddit: "mathrock",
+//   query: "[FRESH ",
+//   sort: "relevance",
+//   time: "week"
+// }
+
+var options = {
+  client_id: process.env.client_id,
+	client_secret: process.env.client_secret,
+	username: process.env.username,
+	password: process.env.password,
+  userAgent: "FRESHBot"
+}
+const r = new snoowrap(options);
+
+// See any recent leaks
+router.get('/search/:subreddit/leaks', function(req, res) {
+  // [LEAK]
+});
+
+router.get('/search/:subreddit', function(req, res) {
+  var albums = [];
+  var eps = [];
+  var mixtapes = [];
+  var videos = [];
+  var songs = [];
+  var other =[];
+  const options = {
+    subreddit: req.params.subreddit,
+    query: "[FRESH ",
+    sort: "relevance",
+    time: "week"
+  }
+  r.search(options).then(response => {
+    response.forEach(function(post) {
+      if (post.title.includes("ALBUM")) {
+        albums.push({ title: post.title, link: post.url});
+      }
+      else if (post.title.includes("MIXTAPE")) {
+        mixtapes.push({ title: post.title, link: post.url});
+      }
+      else if (post.title.includes("EP")) {
+        eps.push({ title: post.title, link: post.url});
+      }
+      else if (post.title.includes("VIDEO")) {
+        videos.push({ title: post.title, link: post.url});
+      }
+      else if (post.title.includes("[FRESH]")) {
+        songs.push({ title: post.title, link: post.url});
+      }
+      // Unknown type
+      else {
+        other.push({ title: post.title, link: post.url});
+      }
+    });
+    var results =  {
+      albums: albums,
+      eps: eps,
+      mixtapes: mixtapes,
+      videos: videos,
+      songs: songs,
+      other: other
+    }
+    res.json(results);
+  });
+});
+
+module.exports = router;
